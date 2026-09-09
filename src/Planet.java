79b56926e76dbfca3d46a990f3128b4201a4164c
asaxena27-cloud <asaxena27@jesuitmail.org>
2026-09-09 13:42:33 -0700

    day1work

---
+++ src/Planet.java
@@ -0,0 +1,27 @@
+public class Planet {
+    double myXPos;
+    double myYPos;
+    double myXVel;
+    double myYVel;
+    double myMass;
+    String myFileName;
+
+    public Planet(double xp, double yp, double xv, double yv, double mass, String filename)
+    {
+        this.myXPos=xp;
+        this.myYPos=yp;
+        this.myXVel=xv;
+        this.myYVel=yv;
+        this.myMass=mass;
+        this.myFileName=filename;
+
+    }
+    public Planet(Planet p)
+    {
+
+    }
+
+
+
+
+    }
